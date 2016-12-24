4e5abc9f4cdb42cb84f08c4dd40413a0abc3c1c8
xala3pa <salazar3pa@gmail.com>
2016-12-24 19:00:37 +0100

    generate a list of random words

--- lambdasStream/src/main/java/exercises/lesson3/RandomWords.java
+++ lambdasStream/src/main/java/exercises/lesson3/RandomWords.java
@@ -29,7 +29,7 @@ public class RandomWords {
    */
   public RandomWords() throws IOException {   
     try (BufferedReader reader = Files.newBufferedReader(Paths.get("words"))) {
-      sourceWords = null;    // YOUR CODE HERE
+      sourceWords = reader.lines().collect(Collectors.toList());
       
       System.out.println("Loaded " + sourceWords.size() + " words");
     }
@@ -43,7 +43,9 @@ public class RandomWords {
    */
   public List<String> createList(int listSize) {
     Random rand = new Random();
-    List<String> wordList = null; // YOUR CODE HERE
+    List<String> wordList = rand.ints(listSize, 0, sourceWords.size())
+            .mapToObj(sourceWords::get)
+            .collect(Collectors.toList());
 
     return wordList;
   }
